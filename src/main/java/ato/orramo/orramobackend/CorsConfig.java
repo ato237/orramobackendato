ddd5152923dfed916db6955f3850566318f31552
ato bradley <tonybradpit@gmail.com>
2021-09-04 00:27:25 +0100

    added war file

--- src/main/java/ato/orramo/orramobackend/CorsConfig.java
+++ src/main/java/ato/orramo/orramobackend/CorsConfig.java
@@ -11,6 +11,6 @@ public class CorsConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry){
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:3001/","https://orramo.herokuapp.com/").allowedMethods("POST", "GET");
+                .allowedOrigins("http://localhost:3000/","https://orramo.herokuapp.com/").allowedMethods("POST", "GET");
     }
 }
--- src/main/java/ato/orramo/orramobackend/OrramobackendApplication.java
+++ src/main/java/ato/orramo/orramobackend/OrramobackendApplication.java
@@ -2,10 +2,18 @@ package ato.orramo.orramobackend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class OrramobackendApplication {
+public class OrramobackendApplication extends SpringBootServletInitializer {
 
+
+
+        @Override
+        protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+            return application.sources(OrramobackendApplication.class);
+        }
     public static void main(String[] args) {
         SpringApplication.run(OrramobackendApplication.class, args);
     }
