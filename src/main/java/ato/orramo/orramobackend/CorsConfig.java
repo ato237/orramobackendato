2e868ee436933c6ab38f87dfecba969dd18c8d9f
ato bradley <tonybradpit@gmail.com>
2021-09-05 16:14:02 +0100

    asdf

--- src/main/java/ato/orramo/orramobackend/CorsConfig.java
+++ src/main/java/ato/orramo/orramobackend/CorsConfig.java
@@ -1,9 +1,16 @@
 package ato.orramo.orramobackend;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.util.Arrays;
+import java.util.Collections;
 
 @Configuration
 @EnableWebMvc
@@ -14,7 +21,19 @@ public class CorsConfig implements WebMvcConfigurer {
                 .allowedOrigins("http://localhost:3000/",
                         "dns1.p08.nsone.net",
                         "orramo.netlify.app",
-                        "https://www.orramo.com/"
+                        "https://www.orramo.com"
                 ).allowedMethods("POST", "GET");
+    }
+    @Bean
+    public CorsFilter corsFilter() {
+        final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        final CorsConfiguration config = new CorsConfiguration();
+        config.setAllowCredentials(true);
+        // Don't do this in production, use a proper list  of allowed origins
+        config.setAllowedOrigins(Collections.singletonList("*"));
+        config.setAllowedHeaders(Arrays.asList("Origin", "Content-Type", "Accept"));
+        config.setAllowedMethods(Arrays.asList("GET", "POST", "OPTIONS"));
+        source.registerCorsConfiguration("/**", config);
+        return new CorsFilter(source);
     }
 }
